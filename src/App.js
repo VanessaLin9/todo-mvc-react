8c865baa7730406e490463bffff99bc46d6c89f3
VanessaLin9 <vanessa7591@gmail.com>
2022-09-17 01:05:54 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import "todomvc-app-css/index.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='app'>
+      <section className='todoapp'>
+        <header>
+          <h1>todos</h1>
+        </header>
+        <input type="text" class="new-todo" autofocus placeholder="需要做什麼？"/>
+
+        <section className='main'>
+          <input id="toggle-all" type="checkbox" class="toggle-all" />
+          <label for="toggle-all"></label>
+
+          <ul className="todo-list">
+
+            <li className='todo'>
+              <div className="view">
+                <input type="checkbox" className='toggle'/>
+                <label for="">學習 React Template</label>
+                <button className="destroy"></button>
+              </div>
+              <input type="text" className="edit" />
+            </li>
+
+            <li className="todo">
+              <div className="view">
+                <input type="checkbox" className="toggle" />
+                <label for="">學習在 React Template 中進行條件判斷</label>
+                <button className="destroy"></button>
+              </div>
+              <input type="text" className="edit" />
+            </li>
+
+            <li className="todo">
+              <div className="view">
+                <input type="checkbox" className="toggle" />
+                <label for="">學習在 React Template 中使用迴圈 </label>
+                <button className="destroy"></button>
+              </div>
+              <input type="text" className="edit" />
+            </li>
+
+          </ul>
+        </section>
+
+        <footer></footer>
+      </section>
     </div>
   );
 }
